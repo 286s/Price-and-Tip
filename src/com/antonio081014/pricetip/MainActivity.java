f7669993eb873dfc3daf2b1adc8dcbec166b6193
Antonio081014 <antonio081014@antonio081014.com>
2013-07-26 20:49:54 -0700

    Remove all imported customized number picker.

--- src/com/antonio081014/pricetip/MainActivity.java
+++ src/com/antonio081014/pricetip/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
